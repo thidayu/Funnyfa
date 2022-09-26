af5ed0745e5240b1af043e8e93d0de6938983300
Thidayu Tangprasertkul <ThidayuT@GuardForce.co.th>
2022-09-26 16:47:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Stack } from "@mui/material";
+
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box>
+        <Stack direction="row" spacing={3} justifyContent="space-evenly">
+      <Sidebar/>
+      <Feed/>
+      <Rightbar/>
+      </Stack>
+      </Box>
     </div>
   );
 }
